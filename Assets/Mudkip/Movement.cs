21f88fc8e29e2bf7a2452c6caae4acfd8d89fde7
565551 <565551@escg.ac.uk>
2026-05-20 15:15:10 +0100

    cheese

---
+++ Assets/Mudkip/Movement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float Hoz;
+    private float Vert;
+    private float OldHoz;
+    private float OldVert;
+
+    public float Speed;
+    private Rigidbody2D RB;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponentInChildren<Animator>();
+    }
+
+	private void Update()
+	{
+		animator.SetFloat("Hoz", Hoz);
+		animator.SetFloat("Vert", Vert);
+		if (Hoz != 0)
+		{
+			animator.SetFloat("OldHoz", Hoz);
+			animator.SetFloat("Vert", 0);
+			animator.SetFloat("OldVert", 0);
+		}
+		if (Vert != 0)
+		{
+			animator.SetFloat("OldVert", Vert);
+			animator.SetFloat("Hoz", 0);
+			animator.SetFloat("OldHoz", 0);
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        Hoz = Input.GetAxisRaw("Horizontal");
+
+		Vert = Input.GetAxisRaw("Vertical");
+
+		RB.velocity = new Vector2(Hoz,Vert).normalized *Speed;
+
+
+    }
+}
